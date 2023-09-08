937f6d8180cdda0fa94cdeabae586dba2791998d
Martín <magadiflo@gmail.com>
2023-09-07 22:45:32 -0500

    Agregando productos en el dashboard

--- refresh-token-angular/src/app/pages/dashboard-page/dashboard-page.component.ts
+++ refresh-token-angular/src/app/pages/dashboard-page/dashboard-page.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { NgFor } from '@angular/common';
+
+import { IResponseProduct } from '../../commons/services/app.model.interface';
+import { AppService } from '../../commons/services/api/app.service';
 
 @Component({
   selector: 'app-dashboard-page',
   standalone: true,
+  imports: [NgFor],
   templateUrl: './dashboard-page.component.html',
   styleUrls: ['./dashboard-page.component.scss']
 })
 export class DashboardPageComponent {
+
+  public products: IResponseProduct[] = [];
+  private _appService = inject(AppService);
+
+  updateTable(): void {
+    this._appService.products()
+      .subscribe(products => this.products = products);
+  }
 
 }
